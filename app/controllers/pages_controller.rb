ffd3519beb87990909a4f292d567cbbe0f786cda
Alexandre BARAT <alex.barat@gmail.com>
2020-01-18 13:53:33 +0100

    coding the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,14 @@ class PagesController < ApplicationController
     header = { 'X-Auth-Token': '6f38ecfa4fc64808badff82fcd366b9d' }
     response = RestClient.get(url, headers = header)
     standings = JSON.parse(response)
+    @standings = standings
   end
 
   def index
+    url = 'http://api.football-data.org/v2/competitions/FL1/matches'
+    header = { 'X-Auth-Token': '6f38ecfa4fc64808badff82fcd366b9d' }
+    response = RestClient.get(url, headers = header)
+    matches = JSON.parse(response)
+    @matches = matches
   end
 end
