b5acc47b64d75577f890d8aa8f59f4ad12c03df9
Alexandre BARAT <alex.barat@gmail.com>
2020-01-20 10:19:42 +0100

    secret key config

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,17 +4,21 @@ require 'json'
 class PagesController < ApplicationController
   def home
     url = 'http://api.football-data.org/v2/competitions/FL1/standings'
-    header = { 'X-Auth-Token': '6f38ecfa4fc64808badff82fcd366b9d' }
+    header = { 'X-Auth-Token': ENV['API_TOKEN'] }
     response = RestClient.get(url, headers = header)
     standings = JSON.parse(response)
-    @standings = standings
+    @standings = standings['standings']
   end
 
   def index
     url = "http://api.football-data.org/v2/teams/#{params["query"]}/matches"
-    header = { 'X-Auth-Token': '6f38ecfa4fc64808badff82fcd366b9d' }
+    header = { 'X-Auth-Token': ENV['API_TOKEN'] }
     response = RestClient.get(url, headers = header)
     matches = JSON.parse(response)
-    @matches = matches
+    @matches = matches['matches']
+    url2 = 'http://api.football-data.org/v2/competitions/FL1/teams'
+    response2 = RestClient.get(url2, headers = header)
+    teams = JSON.parse(response2)
+    @teams = teams['teams']
   end
 end
